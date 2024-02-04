10eb2b069b3a91e8c6945d67ee69a7741210a8e4
Adrien-Ray <a.raymond@codeur.online>
2024-02-04 16:59:07 +0100

    v01

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 // import { caracteres_ascii } from "./caracters_ascii.js";
 import { crypto_v00 } from "./v00/main.js";
+import { crypto_v01 } from "./v01/main.js";
 
 function crypto (stringClear, stringCrypt, pass, version) {
     if ( typeof stringClear === "string" && typeof stringCrypt === "string" && (stringClear === "" || stringCrypt === "") && (stringClear !== "" || stringCrypt !== "") && pass && typeof pass === "string" && version && typeof version === "string") {
+        if (version === "v01") {
+            return crypto_v01(stringClear, stringCrypt, pass);
+        }
         if (version === "v00") {
             return crypto_v00(stringClear, stringCrypt, pass);
-        } else {
+        }
+        else {
             return "error version "+version+" not found";
         }
     } else {
@@ -14,5 +19,7 @@ function crypto (stringClear, stringCrypt, pass, version) {
 };
 
 const test = crypto("abcÿabc","","azertyqwertyazertyqwertyazertyqwerty","v00");
-console.log("resultat de la fonction encode v00 : ", test);
-console.log("resultat de la fonction decode v00 : ", crypto("",test,"azertyqwertyazertyqwertyazertyqwerty","v00"));
+// console.log("resultat de la fonction encode v00 : ", test);
+// console.log("resultat de la fonction decode v00 : ", crypto("",test,"azertyqwertyazertyqwertyazertyqwerty","v00"));
+
+export { crypto };
---
+++ v01/main.js
@@ -0,0 +1,81 @@
+import { caracteres_ascii } from "../caracters_ascii.js";
+
+// [X] ici on va transformer la function sommeOfPass pour qu'elle retourne un tableau de sommes de pass
+
+// ensuite dans encodeCaractere et decodeCaractere on va double-boucler pour "sommer" le tableau a chiffrer / déchiffrer dans un incrément qui boucle conjointement / dans le tableau des sommes de pass
+
+export function crypto_v01(stringClear, stringCrypt, pass) {
+    if (stringClear) {
+        const stringInArray = stringToArray(stringClear);
+        const passArray = stringToArray(pass);
+        let result = "";
+        const passSomme = sommeOfPass(passArray);
+        /* for (const element of stringInArray) {
+            result = result + encodeCaractere(element, passSomme);
+        }; */
+        for (let i = 0; i < stringInArray.length; i++) {
+            const element = stringInArray[i];
+            const passSommeElement = passSomme[(i + passSomme.length) % passSomme.length];
+            result = result + encodeCaractere(element, passSommeElement);      
+        }
+        return result;
+    } if (stringCrypt) {
+        // console.info('to function déchiffre');
+        const stringInArray = stringToArray(stringCrypt);
+        const passArray = stringToArray(pass);
+        // console.log(stringInArray, passArray);
+        let result = "";
+        const passSomme = sommeOfPass(passArray);
+        /* for (const element of stringInArray) {
+            result = result + decodeCaractere(element, passSomme);
+        }; */
+        for (let i = 0; i < stringInArray.length; i++) {
+            const element = stringInArray[i];
+            const passSommeElement = passSomme[(i + passSomme.length) % passSomme.length];
+            result = result + decodeCaractere(element, passSommeElement);      
+        }
+        return result;
+    }
+    return 'error wrong way round';
+}
+
+function stringToArray(string) {
+    return string.split('');
+}
+
+function encodeCaractere(stringElement, passSomme) {
+    // in : caractere a encoder, somme pass
+        let result = caracteres_ascii.findIndex(function(element) {
+            return element.char === stringElement;
+        });
+        result = result + passSomme;
+    // console.log("result in encodeCaractere : ", result, caracteres_ascii[result].char);
+    return caracteres_ascii[result % caracteres_ascii.length].char;
+}
+
+function decodeCaractere(stringElement, passSomme) {
+    // in : caractere a decoder, somme pass
+        let result = caracteres_ascii.findIndex(function(element) {
+            return element.char === stringElement;
+        });
+    // console.log("result in encodeCaractere : ", result, caracteres_ascii[result].char);
+    return caracteres_ascii[(result - passSomme + caracteres_ascii.length) % caracteres_ascii.length].char;
+}
+
+function sommeOfPass(passArray) {
+    // in : pass array, out : number
+    // console.log('in sommeOfPass', passArray);
+    let passSome = [];
+    for (const elementPassArray of passArray) {
+        let passSomeOne = caracteres_ascii.findIndex(function(element) {
+            return element.char === elementPassArray;
+        });
+        // console.log('in sommeOfPass for', passSomeOne);
+        // passSome = passSome + passSomeOne;
+        passSome.push(passSomeOne + caracteres_ascii.length % caracteres_ascii.length);
+    };
+    // console.log('in sommeOfPass result', passSome);
+    console.log('sommeOfPass return : ', passSome);
+    return passSome;
+    // return 1;
+}
