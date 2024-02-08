878338e39d0262948f78fadc067c9055091e230b
Adrien-Ray <a.raymond@codeur.online>
2024-02-08 20:48:22 +0100

    v03 ready

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ crypto("chaîne à chiffrer", "chaîne à déchiffrer", "password", "version");
 // la v00 est un chiffre de César (extrêmement simple)
 // la v01 est un chiffre de César dont le décalage change d'un caractère à l'autre, selon une période égale à la longueur du mot de passe
 // la v02 ajoute un incrément dans le décalage des caractère
-// la v03 implémente un second système de décalage.
+// la v03 implémente un second système de décalage. Décalage d'un système de roues incrémentales dont chacune remonte la suivante de 1 a chaque tour, dont la position initial et le nombre est fixé par le password afin d'éliminer un maximum de répétitions dans la chaîne chiffrée.
 
 // exemple encode :
 crypto("abcÿabc","","azertyqwertyazertyqwertyazertyqwerty","v00");
--- v03/main.js
+++ v03/main.js
@@ -1,5 +1,30 @@
 import { caracteres_ascii } from "../caracters_ascii.js";
 
+console.log(caracteres_ascii);
+
+let roues = [];
+
+function setRoues(passSome) {
+    // in: array of pass values
+    // set the roues
+    console.log('setRoues : ', passSome);
+    roues = passSome;
+}
+
+function incrementRoues() {
+    // increment des roues
+    roues[0] = roues[0] + 1;
+    if (roues[0] >= caracteres_ascii.length) {
+        for (let i = 0; i < roues.length - 1; i++) {
+            // const element = roues[i];
+            if (roues[i] >= caracteres_ascii.length) {
+                roues[i] = 0;
+                roues[i+1] = roues[i+1] + 1;
+            }
+        }
+    };
+}
+
 let objCompteur = {
     _compteur: 0, // init
     set compteur(nouvelleValeur) {
@@ -9,9 +34,7 @@ let objCompteur = {
     get compteur() {
         return this._compteur;
     },
-    trigger() {
-        console.log('La variable a changé de valeur : ', this._compteur);
-    }
+    trigger() {}
 };
 
 // ex déclenchement
@@ -29,7 +52,13 @@ export function crypto_v03(stringClear, stringCrypt, pass) {
         const passSommeElement = passSomme[(i + passSomme.length) % passSomme.length];
         let sens = "";
         if (stringClear) { sens = "chiffre"; } else if (stringCrypt) { sens = "dechiffre"; }
-        result = result + encodeDecodeCaractere(element, (passSommeElement + decalageIncremental + caracteres_ascii.length) % caracteres_ascii.length, sens);
+        console.log('passSomme before', passSomme);
+        result = result + encodeDecodeCaractere(
+            element,
+            (passSommeElement + decalageIncremental + roues[(i + roues.length) % roues.length] - 1 + caracteres_ascii.length) % caracteres_ascii.length,
+            sens
+        );
+        console.log('passSomme after', passSomme);
     }
     clearGlobal();
     return result;
@@ -47,6 +76,7 @@ function encodeDecodeCaractere(stringElement, passSomme, sens) {
     let switchSens = (sens === "chiffre") ? result + passSomme : result - passSomme;
     result = ((switchSens) + caracteres_ascii.length) % caracteres_ascii.length;
     objCompteur.compteur++;
+    incrementRoues();
     return caracteres_ascii[result].char;
 }
 
@@ -59,9 +89,11 @@ function sommeOfPass(passArray) {
         });
         passSome.push(passSomeOne + caracteres_ascii.length % caracteres_ascii.length);
     };
+    setRoues(passSome);
     return passSome;
 }
 
 function clearGlobal() {
     objCompteur.compteur = 0;
+    roues = [];
 }
